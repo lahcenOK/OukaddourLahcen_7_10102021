0847dc576b51d7b787b91da871a1e258c579675f
oukaddour <lawsen@hotmail.fr>
2021-10-19 07:11:41 +0200

    modifictation fichier App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,88 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Groupomania</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation" >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/">Home</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Compte
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <router-link class="nav-link" to="/profile"
+                  ><i class="fas fa-user-edit"></i> Profile</router-link
+                >
+              </li>
+              <li>
+                <router-link class="nav-link" to="/login"
+                  ><i class="fas fa-sign-in-alt"></i> Se Connecter</router-link
+                >
+              </li>
+              <li>
+                <router-link class="nav-link" to="/login" v-on:click="Logout()"
+                  ><i class="fas fa-sign-out-alt"></i> Déconnexion</router-link
+                >
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <router-link class="btn btn-outline-secondary" to="/inscription">
+          <i class="fas fa-edit"></i> Inscription
+        </router-link>
+        <router-link class="btn btn-outline-info" to="/about">
+          <i class="fas fa-info"></i>
+        </router-link>
+      </div>
+    </div>
+  </nav>
+  <router-view />
+  <footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+      <span class="text-muted">Place sticky footer content here.</span>
+    </div>
+  </footer>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  data() {
+    return {
+      token: "",
+      allMessages: [],
+    };
+  },
+  methods: {
+    Logout() {
+      localStorage.clear();
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,17 +90,13 @@
   text-align: center;
   color: #2c3e50;
 }
+.btn {
+  margin-right: 10px;
+}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+footer {
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
 }
 </style>
