e79836ebd983acb0a2582416b8d42aa1da153bf2
oukaddour <lawsen@hotmail.fr>
2021-10-29 18:37:21 +0200

    Ajout de supp. commentaire

---
+++ frontend/src/components/deleteComment.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button type="submit" @click.prevent="deleteComment"><i class="fas fa-trash-alt"></i></button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "deleteComment",
+  props: {
+    idComm: Number,
+  },
+  data() {
+    return {
+      token: "",
+      isAdmin: "",
+      id: this.$route.params.id,
+    };
+  },
+  methods: {
+    deleteComment() {
+      let token = localStorage.getItem("token");
+      axios
+        .delete(
+          "http://localhost:3000/api/posts/" +
+            this.id +
+            "/comment/" +
+            this.idComm,
+          {
+            headers: { Authorization: "Bearer " + token },
+          }
+        )
+        .then(() => {
+          alert("Votre commentaire a bien été supprimé !");
+          document.location.reload();
+        })
+        .catch((error) => {
+          console.log({ error });
+        });
+    },
+  },
+};
+</script>
